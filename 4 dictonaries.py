3ef059d5382cb645ac14c800cfbbb519a600005b
Jingwen Zhou <112013227+jing3508@users.noreply.github.com>
2022-11-07 18:21:00 -0500

    Add files via upload

---
+++ 4 dictonaries.py
@@ -0,0 +1,13 @@
+scraping_dict = {'title': "NASA's Perseverance Rover Bringing 3D-Printed Metal Parts to Mars", 'preview': "For hobbyists and makers, 3D printing expands creative possibilities; for specialized engineers, it's also key to next-generation spacecraft design."}
+print(scraping_dict)
+
+scraping_dict_2 = {'title': "NASA Updates Mars 2020 Mission Environmental Review", 'preview': "'NASA and the Department of Energy have completed a more detailed risk analysis for the Mars 2020 rover launch from Florida."}
+print(scraping_dict_2)
+
+scraping_dict_3 = {'title': "Join NASA for the Launch of the Mars 2020 Perseverance Rover", 'preview': "'No matter where you live, choose from a menu of activities to join NASA as we 'Countdown to Mars' and launch the Perseverance rover to the Red Planet."}
+print(scraping_dict_3)
+
+scraping_dict_4 = {'title': "AI Is Helping Scientists Discover Fresh Craters on Mars", 'preview': "'It's the first time machine learning has been used to find previously unknown craters on the Red Planet."}
+print(scraping_dict_4)
+
+
